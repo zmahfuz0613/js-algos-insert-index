773b9c7129aa70c96adb0fe2ab8718f9daea627e
zmahfuz061318 <str053mgr@gmail.com>
2020-07-21 17:04:52 -0400

    Work on progress

--- __tests__/test.js
+++ __tests__/test.js
@@ -1,7 +1,7 @@
 let algos = require("../getIndexToInsert.js");
 
 //INSERT INDEX
-
+// work on progress
 test("find the correct index to insert the given number", () => {
   expect(algos.getIndexToInsert([40, 60, 5], 6)).toBe(1);
   expect(algos.getIndexToInsert([40, 60, 5], -1)).toBe(0);
