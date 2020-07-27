83ae3c38c1755b7f3319c9c41df83a8be5029c32
zmahfuz061318 <str053mgr@gmail.com>
2020-07-26 20:58:31 -0400

    Completed

--- getIndexToInsert.js
+++ getIndexToInsert.js
@@ -2,6 +2,12 @@
 
 const getIndexToInsert = (arr, num) => {
 
+  let sortedArray = arr.sort((a,b)=> a-b);
+  let indexToInsert = sortedArray.findIndex((a)=> a >=num);
+  if(indexToInsert === -1){
+    return sortedArray.length;
+  }
+  return indexToInsert;
 };
 
 console.log(getIndexToInsert([40, 60, 5, 322, 48, 83], 80));
